e2d0ff30e5fc3781bd64ee8a78a7a2af6db1135e
sinha97 <vk9633698@gmail.com>
2021-01-27 11:28:07 +0530

    try to remove bug

--- client/src/componnents/profile/ProfileGithub.js
+++ client/src/componnents/profile/ProfileGithub.js
@@ -4,20 +4,20 @@ import {connect} from 'react-redux';
 import Spinner from '../layout/Spinner';
 import {getGithubRepos} from '../../actions/profile';
 
-const ProfileGithub = ({username,getGithubRepos,repos}) => {
+const ProfileGithub = ({ username, getGithubRepos, repos }) => {
     useEffect(() => {
         getGithubRepos(username);
-    },[getGithubRepos]);
+    }, [getGithubRepos, username]);
 
     return (
         <div className="profile-github">
-            <h2 className = 'text-primary my-1'>Github Repos</h2>
+            <h2 className='text-primary my-1'>Github Repos</h2>
             {repos === null ? (<Spinner />) : (
-                repos.map(repo =>(
-                    <div key = {repo._id} className='repo bg-white p-1 my-1'>
+                repos.map(repo => (
+                    <div key={repo._id} className='repo bg-white p-1 my-1'>
                         <div>
                             <h4>
-                                <a href = {repo.html_url} target="_blank" rel="noopener noreferrer" >
+                                <a href={repo.html_url} target="_blank" rel="noopener noreferrer" >
                                     {repo.name}
                                 </a>
                             </h4>
@@ -41,17 +41,17 @@ const ProfileGithub = ({username,getGithubRepos,repos}) => {
             )}
             
         </div>
-    )
-}
+    );
+};
 
 ProfileGithub.propTypes = {
     getGithubRepos: PropTypes.func.isRequired,
-    repos:PropTypes.array.isRequired,
+    repos: PropTypes.array.isRequired,
     username: PropTypes.string.isRequired,
-}
+};
 
 const mapStateToProps = state => ({
     repos: state.profile.repos
-})
+});
 
-export default connect(mapStateToProps,{getGithubRepos})(ProfileGithub)
+export default connect(mapStateToProps, { getGithubRepos })(ProfileGithub);
